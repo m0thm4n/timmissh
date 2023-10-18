861a757fa019b1567b4cda7fa4815e9963be3e42
Nathan Moritz <nateim3@gmail.com>
2023-10-18 15:39:21 -0400

    New code

--- main.go
+++ main.go
@@ -1,197 +1,117 @@
 package main
 
+// An example git server. This will list all available repos if you ssh
+// directly to the server. To test `ssh -p 23233 localhost` once it's running.
+
 import (
 	"context"
+	"errors"
 	"fmt"
-	"log"
+	"io/fs"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 
-	"github.com/charmbracelet/bubbles/textarea"
-	"github.com/charmbracelet/bubbles/viewport"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
-	bm "github.com/charmbracelet/wish/bubbletea"
+	gm "github.com/charmbracelet/wish/git"
 	lm "github.com/charmbracelet/wish/logging"
-	"github.com/muesli/termenv"
 )
 
 const (
-	host = "localhost"
-	port = 23234
+	port    = 23233
+	host    = "localhost"
+	repoDir = ".repos"
 )
 
-// app contains a wish server and the list of running programs.
 type app struct {
-	*ssh.Server
-	progs []*tea.Program
+	access gm.AccessLevel
 }
 
-// send dispatches a message to all running programs.
-func (a *app) send(msg tea.Msg) {
-	for _, p := range a.progs {
-		go p.Send(msg)
-	}
+func (a app) AuthRepo(repo string, pk ssh.PublicKey) gm.AccessLevel {
+	return a.access
 }
 
-func newApp() *app {
-	a := new(app)
+func (a app) Push(repo string, pk ssh.PublicKey) {
+	log.Info("push", "repo", repo)
+}
+
+func (a app) Fetch(repo string, pk ssh.PublicKey) {
+	log.Info("fetch", "repo", repo)
+}
+
+func passHandler(ctx ssh.Context, password string) bool {
+	return false
+}
+
+func pkHandler(ctx ssh.Context, key ssh.PublicKey) bool {
+	return true
+}
+
+func main() {
+	// A simple GitHooks implementation to allow global read write access.
+	a := app{gm.ReadWriteAccess}
 
 	s, err := wish.NewServer(
+		ssh.PublicKeyAuth(pkHandler),
+		ssh.PasswordAuth(passHandler),
 		wish.WithAddress(fmt.Sprintf("%s:%d", host, port)),
-		wish.WithHostKeyPath(".ssh/term_info_ed25519"),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
 		wish.WithMiddleware(
-			bm.MiddlewareWithProgramHandler(a.ProgramHandler, termenv.ANSI256),
+			gm.Middleware(repoDir, a),
+			gitListMiddleware,
 			lm.Middleware(),
 		),
 	)
-
 	if err != nil {
-		log.Fatalln(err)
+		log.Error("could not start server", "error", err)
 	}
 
-	a.Server = s
-	return a
-}
-
-func (a *app) Start() {
-	var err error
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	log.Printf("Starting SSH server on %s:%d", host, port)
+	log.Info("Starting SSH server", "host", host, "port", port)
 	go func() {
-		if err = a.ListenAndServe(); err != nil {
-			log.Fatalln(err)
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("could not start server", "error", err)
+			done <- nil
 		}
 	}()
 
 	<-done
-	log.Println("Stopping SSH server")
+	log.Info("Stopping SSH server")
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer func() { cancel() }()
-	if err := a.Shutdown(ctx); err != nil {
-		log.Fatalln(err)
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("could not stop server", "error", err)
 	}
 }
 
-func (a *app) ProgramHandler(s ssh.Session) *tea.Program {
-	if _, _, active := s.Pty(); !active {
-		wish.Fatalln(s, "terminal is not active")
-	}
-
-	model := initialModel()
-	model.app = a
-	model.id = s.User()
-
-	p := tea.NewProgram(model, tea.WithOutput(s), tea.WithInput(s))
-	a.progs = append(a.progs, p)
-
-	return p
-}
-
-func main() {
-	app := newApp()
-	app.Start()
-}
-
-type (
-	errMsg  error
-	chatMsg struct {
-		id   string
-		text string
-	}
-)
-
-type model struct {
-	*app
-	viewport    viewport.Model
-	messages    []string
-	id          string
-	textarea    textarea.Model
-	senderStyle lipgloss.Style
-	err         error
-}
-
-func initialModel() model {
-	ta := textarea.New()
-	ta.Placeholder = "Send a message..."
-	ta.Focus()
-
-	ta.Prompt = "┃ "
-	ta.CharLimit = 280
-
-	ta.SetWidth(30)
-	ta.SetHeight(3)
-
-	// Remove cursor line styling
-	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
-
-	ta.ShowLineNumbers = false
-
-	vp := viewport.New(30, 5)
-	vp.SetContent(`Welcome to the chat room!
-Type a message and press Enter to send.`)
-
-	ta.KeyMap.InsertNewline.SetEnabled(false)
-
-	return model{
-		textarea:    ta,
-		messages:    []string{},
-		viewport:    vp,
-		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
-		err:         nil,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return textarea.Blink
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		tiCmd tea.Cmd
-		vpCmd tea.Cmd
-	)
-
-	m.textarea, tiCmd = m.textarea.Update(msg)
-	m.viewport, vpCmd = m.viewport.Update(msg)
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC, tea.KeyEsc:
-			return m, tea.Quit
-		case tea.KeyEnter:
-			m.app.send(chatMsg{
-				id:   m.id,
-				text: m.textarea.Value(),
-			})
-			m.textarea.Reset()
+// Normally we would use a Bubble Tea program for the TUI but for simplicity,
+// we'll just write a list of the pushed repos to the terminal and exit the ssh
+// session.
+func gitListMiddleware(h ssh.Handler) ssh.Handler {
+	return func(s ssh.Session) {
+		// Git will have a command included so only run this if there are no
+		// commands passed to ssh.
+		if len(s.Command()) == 0 {
+			des, err := os.ReadDir(repoDir)
+			if err != nil && err != fs.ErrNotExist {
+				log.Error("invalid repository", "error", err)
+			}
+			if len(des) > 0 {
+				fmt.Fprintf(s, "\n### Repo Menu ###\n\n")
+			}
+			for _, de := range des {
+				fmt.Fprintf(s, "• %s - ", de.Name())
+				fmt.Fprintf(s, "git clone ssh://%s:%d/%s\n", host, port, de.Name())
+			}
+			fmt.Fprintf(s, "\n\n### Add some repos! ###\n\n")
+			fmt.Fprintf(s, "> cd some_repo\n")
+			fmt.Fprintf(s, "> git remote add wish_test ssh://%s:%d/some_repo\n", host, port)
+			fmt.Fprintf(s, "> git push wish_test\n\n\n")
 		}
-
-	case chatMsg:
-		m.messages = append(m.messages, m.senderStyle.Render(msg.id)+": "+msg.text)
-		m.viewport.SetContent(strings.Join(m.messages, "\n"))
-		m.viewport.GotoBottom()
-
-	// We handle errors just like any other message
-	case errMsg:
-		m.err = msg
-		return m, nil
+		h(s)
 	}
-
-	return m, tea.Batch(tiCmd, vpCmd)
-}
-
-func (m model) View() string {
-	return fmt.Sprintf(
-		"%s\n\n%s",
-		m.viewport.View(),
-		m.textarea.View(),
-	) + "\n\n"
 }
